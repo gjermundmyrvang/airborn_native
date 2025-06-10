543eaa0ef079a830c7badc54a46e67d2df164583
gjermundmyrvang <myrvang.gjermund@gmail.com>
2025-06-10 13:14:58 +0200

    init project

---
+++ theme/theme.ts
@@ -0,0 +1,19 @@
+import { MD3LightTheme as DefaultTheme } from 'react-native-paper';
+
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1B263B',     // Blue
+    onPrimary: '#ffffff',
+    secondary: '#415A77',   // Bluesecond
+    onSecondary: '#ffffff',
+    tertiary: '#F4A261',    // Orange
+    background: '#0D1B2A',  // Dark blue
+    onBackground: '#ffffff',
+    surface: '#1E2A3A',     // dark surface
+    onSurface: '#ffffff',
+    error: '#EF4444',      
+  },
+};
