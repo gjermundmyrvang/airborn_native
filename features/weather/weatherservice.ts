153c32c56a73c3e1f25473c71d5519a46be7618b
gjermundmyrvang <myrvang.gjermund@gmail.com>
2025-06-11 19:54:12 +0200

    change to just returning the relevant data

--- features/weather/weatherservice.ts
+++ features/weather/weatherservice.ts
@@ -3,7 +3,7 @@ import { LatLon } from '../../types/CommonTypes';
 import { WeatherApiResponse } from './types';
 
 export const getWeatherData = async (pos: LatLon): Promise<WeatherApiResponse> => {
-  const response = await axios.get(`/locationforecast/2.0/complete?lat=${pos.latitude}&lon=${pos.longitude} `);
+  const response = await axios.get(`/locationforecast/2.0/complete?lat=${pos.latitude}&lon=${pos.longitude}`);
   console.log("Fetching weatherdata:")
-  return response.data;
+  return response.data.properties.timeseries;
 };
