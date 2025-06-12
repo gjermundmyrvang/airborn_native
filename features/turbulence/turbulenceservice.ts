a0fdc39cf1d5bea55e4c05e0aeac681520eaaebc
gjermundmyrvang <myrvang.gjermund@gmail.com>
2025-06-12 14:54:10 +0200

    fetch all turbulence data from api

--- features/turbulence/turbulenceservice.ts
+++ features/turbulence/turbulenceservice.ts
@@ -1,5 +1,7 @@
-import { TurbulenceProps } from './types';
+import axios from '../../api/axiosInstance';
+import { TurbulenceResponse } from './types';
 
-export const getTurbulenceData = async (props: TurbulenceProps) => {
-  return `https://api.met.no/weatherapi/turbulence/2.0/?icao=${props.icao}&type=${props.type}`;
+export const getTurbulenceData = async (icao: string): Promise<TurbulenceResponse> => {
+  const response = await axios.get(`https://api.met.no/weatherapi/turbulence/2.0/available.json?icao=${icao}`)
+  return response.data;
 };
