bf177923cadee55578860f9cf92ac92234787c75
gjermundmyrvang <myrvang.gjermund@gmail.com>
2025-06-12 14:28:53 +0200

    change navigation types to support new logic

--- navigation/types.ts
+++ navigation/types.ts
@@ -1,19 +1,12 @@
 import { Airport } from "../data/airports";
 
 export type RootStackParamList = {
-  Home: undefined;
-  Flightscreen: {
-    departure: Airport;
-    arrival?: Airport | null;
-  };
+  Homescreen: undefined;
+  Flightscreen: undefined;
 };
 
 export type FlightscreenTabParamList = {
-  Departure: { departure: Airport };
-  Arrival: { arrival?: Airport | null };
-  Overall: { departure: Airport; arrival?: Airport | null };
-  AirportTab: {
-    departure?: Airport | null;
-    arrival?: Airport | null;
-  };
+  DepartureTab: { departure?: Airport };
+  ArrivalTab: { arrival?: Airport };
+  Overall: { departure?: Airport; arrival?: Airport };
 };
