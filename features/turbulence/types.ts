44d79af18b2147564b42ce499c33c8d361c4698a
gjermundmyrvang <myrvang.gjermund@gmail.com>
2025-06-12 14:53:54 +0200

    change types to fit new response

--- features/turbulence/types.ts
+++ features/turbulence/types.ts
@@ -1,4 +1,12 @@
-export type TurbulenceProps = {
-    icao: string;
-    type: string;
+export type TurbulenceParams = {
+    icao: string; 
+    time: string; 
+    type: "cross_section" | "map";
 }
+
+export type TurbulenceEntry = {
+    params: TurbulenceParams;
+    uri: string;
+}
+
+export type TurbulenceResponse = TurbulenceEntry[];
