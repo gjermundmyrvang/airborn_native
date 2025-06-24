7235a712a390bcdfe47af83f0966c956ca509b51
gjermundmyrvang <myrvang.gjermund@gmail.com>
2025-06-24 21:47:33 +0200

    add forecast to overall if dep and arr is an iga route one way or the other

--- screens/tabs/OverallTab.tsx
+++ screens/tabs/OverallTab.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView } from "react-native";
 import { useTheme } from "react-native-paper";
 import { DistanceInfoBox } from "../../components/DistanceInfo";
@@ -6,11 +6,28 @@ import Sigchart from "../../features/sigchart/Sigchart";
 import { useFlightStore } from "../../utils/flightStore";
 import { GeoSat } from "../../features/geosat/GeoSat";
 import { RadarImages } from "../../features/radar/Radar";
+import {
+  createRoute,
+  isIgaRoute,
+} from "../../features/routeforecast/routeservice";
+import RouteForecast from "../../features/routeforecast/RouteForecast";
 
 export default function OverallTab() {
+  const [igaRoute, setIgaRoute] = useState<string | null>(null);
   const departure = useFlightStore((s) => s.depAirport);
   const arrival = useFlightStore((s) => s.arrAirport);
   const { colors } = useTheme();
+
+  useEffect(() => {
+    if (!arrival || !departure) return;
+    const checkRoute = () => {
+      if (isIgaRoute(departure.icao, arrival.icao)) {
+        setIgaRoute(createRoute(departure.icao, arrival.icao));
+      }
+    };
+    checkRoute();
+  }, []);
+
   return (
     <ScrollView
       style={{ flex: 1, backgroundColor: colors.background }}
@@ -22,6 +39,7 @@ export default function OverallTab() {
       <Sigchart />
       <GeoSat />
       <RadarImages />
+      {igaRoute && <RouteForecast route={igaRoute} />}
     </ScrollView>
   );
 }
